f7468444a382cc6442d6c4ea803e00b742876708
mingguk <smk7136@gmail.com>
2022-01-25 15:51:33 +0900

    JS navbar menu item clicked

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight : ${navbarHeight}`);
+    // console.log(window.scrollY);
+    // console.log(`navbarHeight : ${navbarHeight}`);
 
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
@@ -13,3 +13,20 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 })
+
+// Handle scrolloing when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', () => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(link)
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+})
+
+
