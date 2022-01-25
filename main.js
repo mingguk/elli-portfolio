6d932beb811d9bf621196302bd4b24647b9447c4
mingguk <smk7136@gmail.com>
2022-01-25 22:32:38 +0900

    JS arrow-up

--- main.js
+++ main.js
@@ -46,9 +46,32 @@ document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 })
 
+// Show 'arrow up' button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Move to home when arrow-up button clicked
+
+arrowUp.addEventListener('click', () => {
+    scrollIntoViews('#home');
+})
+
 
 
 function scrollIntoViews(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
 }
+
+// make Up arrow button
+
+//1. 홈 세로 길이밑으로 내려가면 우측 하단에 애로우 키 추가
+//2. 홈세로 길이 안으로 들어오면 우측하단 애로우 키 감추기
+//3. 애로우키 누르면 홈, 최상단으로 이동시키기
+
